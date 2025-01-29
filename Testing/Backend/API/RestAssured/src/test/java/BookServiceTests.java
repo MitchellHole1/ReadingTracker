9a68503968175ad11917862b388bb952028d13c1
MitchellHole1 <mitchell.hole1@gmail.com>
2025-01-29 15:40:45 -0800

    more tests

--- Testing/Backend/API/RestAssured/src/test/java/BookServiceTests.java
+++ Testing/Backend/API/RestAssured/src/test/java/BookServiceTests.java
@@ -35,7 +35,7 @@ public class BookServiceTests {
         then().
             statusCode(HttpStatus.SC_OK).
             body("id", equalTo(1),
-            "name", equalTo("Blood Meridian"),
+                "name", equalTo("Blood Meridian"),
                     "yearPublished", equalTo(1985),
                     "author.name", equalTo("Cormac McCarthy"));
     }
@@ -46,6 +46,47 @@ public class BookServiceTests {
             get("/100").
         then().
             statusCode(HttpStatus.SC_NOT_FOUND);
+    }
+
+    @Test
+    public void create_book_returns_200_and_created_book() {
+        given().
+            body("{\"name\": \"The Orchard Keeper\", \"yearPublished\": 1965, \"authorId\": 1, \"pages\": 212, \"type\": \"Novel\", \"originalLanguage\": \"English\"}").
+            header("Content-Type", "application/json").
+        when().
+            post("/").
+        then().
+            statusCode(HttpStatus.SC_OK).
+            body("name", equalTo("The Orchard Keeper"),
+                "yearPublished", equalTo(1965),
+                "originalLanguage", equalTo("English"),
+                "pages", equalTo(212),
+                "type", equalTo("Novel"),
+                "author.name", equalTo("Cormac McCarthy")
+        );
+    }
+
+    @Test
+    public void create_book_with_missing_fields_returns_400() {
+        given().
+            body("{\"name\": \"The Orchard Keeper\", \"yearPublished\": 1965, \"authorId\": 1}").
+            header("Content-Type", "application/json").
+        when().
+            post("/").
+        then().
+            statusCode(HttpStatus.SC_BAD_REQUEST);
+    }
+
+    @Test
+    public void create_book_with_invalid_author_id_returns_400() {
+        given().
+            body("{\"name\": \"The Orchard Keeper\", \"yearPublished\": 1965, \"authorId\": 100, \"originalLanguage\": \"English\"}").
+            header("Content-Type", "application/json").
+        when().
+            post("/").
+        then().
+            statusCode(HttpStatus.SC_BAD_REQUEST).
+            extract().response().body().asString().contains("Author not found");
     }
 
 }
