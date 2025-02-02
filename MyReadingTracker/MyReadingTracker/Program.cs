3c114533260cdf46a5068a9b4cec469aac32649f
MitchellHole1 <mitchell.hole1@gmail.com>
2025-02-02 11:10:27 -0800

    Update

--- MyReadingTracker/MyReadingTracker/Program.cs
+++ MyReadingTracker/MyReadingTracker/Program.cs
@@ -1,5 +1,7 @@
 using System.Text.Json.Serialization;
 using ContosoPizza.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using MyReadingTracker.Data;
 using MyReadingTracker.Services;
 
@@ -17,7 +19,21 @@ builder.Services.AddControllers().AddJsonOptions(options =>
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSqlite<LibraryContext>("Data Source=MyLibrary.db");
+
+builder.Services.AddDbContext<LibraryContext>((sp, options) =>
+{
+    if (builder.Environment.IsEnvironment("Local"))
+    {
+        options.UseInMemoryDatabase("library-api-in-memory");
+    }
+    else
+    {
+        var connectionString = builder.Configuration.GetConnectionString("Postgres");
+        options.UseNpgsql(connectionString);
+    }
+    options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
+});
+
 builder.Services.AddScoped<IBookService, BookService>();
 builder.Services.AddScoped<IAuthorService, AuthorService>();
 builder.Services.AddScoped<IReadingSessionService, ReadingSessionService>();
